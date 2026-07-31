d16c9a502dfb4bc0a5cf6ee1142e7a281ede6ba4
goldenapples <goldenapplesdesign@gmail.com>
2026-07-31 15:31:51 -0400

    Bump version number to 0.1.1

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WP Pattern Library
  * Description: Serves a manifest and isolated previews of the site's registered block patterns.
- * Version: 0.1.0
+ * Version: 0.1.1
  * License: GPL-2.0-or-later
  * Requires PHP: 8.1
  *
